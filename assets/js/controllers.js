871b87f96933cc73b62e4978f1fd2c4c3caeaf72
Lisss13 <mironovarseniy@gmail.com>
2016-02-26 18:09:51 +0300

    Версия 1.0

---
+++ assets/js/controllers.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* Controllers */
+
+var sportShop = angular.module('sportShop', []);
+
+  sportShop.controller('shopCtrl',['$scope','$http', function($scope, $http) {
+    $scope.title = 'скандинавская ходьба';
+
+    $http.get('product/shop.json').success(function(data, status, headers, config) {
+        $scope.phones = data;
+    });
+
+    var date = new Date;
+
+    $scope.today = date;
+}]);
